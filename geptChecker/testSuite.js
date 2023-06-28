d9840a3b05611be76354e3061d1cdaf816b86960
eccomaggio <eccomaggio@gmail.com>
2023-06-28 18:33:04 +0100

    fixed problem with compounds at end of text or individually

---
+++ geptChecker/testSuite.js
@@ -0,0 +1,12 @@
+// document.addEventListener("DOMContentLoaded", tests);
+// document.addEventListener("DOMContentLoaded", (event) => {
+//   console.log("DOM fully loaded and parsed");
+// 	console.log("placeholder :p")
+// });
+
+function testSuite(){
+	console.log("Placeholder for tests.");
+}
+
+// alert("hello!");
+// testSuite();
