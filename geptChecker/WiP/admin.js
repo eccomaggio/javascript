9d8097c21454aaea76bfa2d59cb7829ceebc680c
eccomaggio <eccomaggio@gmail.com>
2024-04-22 15:15:44 +0100

    fixed visibleLevelLimit strikethrough feature

--- geptChecker/WiP/admin.js
+++ geptChecker/WiP/admin.js
@@ -1,11 +1,8 @@
     console.clear();
     "use strict";
-    // document.getElementById("version-no").innerHTML = "Version 5-0.2"
-    const versionNumber = "6-3.0";
-    const versionDetails = "New GEPT wordlist; edits 'in place'; & advises on preferred spellings; recognizes negative prefixes and superfluous hyphens";
+    const versionNumber = "6-4.0";
+    const versionDetails = "New GEPT wordlist; edits 'in place'; advises on preferred spellings; recognizes negative prefixes and superfluous hyphens; custom level strike-through";
     const versionInfo = document.getElementById("version-no");
-    // versionDetails.innerHTML = "Version 6-1.1";
-    // versionDetails.title += "\nIncorporates new GEPT wordlist and British spelling.";
     versionInfo.innerHTML = `Version ${versionNumber}`;
     versionInfo.title += `\n${versionDetails}.`;
     document.title = `LTTC Wordlist Tool (V.${versionNumber})`;
