77d32f1d6a8e33f99ee2338bf119186439cfc2d6
eccomaggio <eccomaggio@gmail.com>
2024-09-25 13:25:36 +0100

    refactor: kids show displaystats; fix: kids diswordsearch display; fix: (maybe) refresh text sometimes results in refresh required display

--- geptChecker/WiP/admin.js
+++ geptChecker/WiP/admin.js
@@ -1,7 +1,7 @@
-    console.clear();
     "use strict";
-    const versionNumber = "6-5.13";
-    const versionDetails = "*New GEPT wordlist; edits 'in place'; *advises on preferred spellings; *recognizes negative prefixes and superfluous hyphens; *custom level strike-through; *preserves original spacing/punctuation;\n[nerdy]\n+all html output now escaped through Tag.stringify(); +implemented all functions as classes; +font size now saved between sessions";
+    console.clear();
+    const versionNumber = "6-5.14";
+    const versionDetails = "*New GEPT wordlist; edits 'in place'; *advises on preferred spellings; *recognizes negative prefixes and superfluous hyphens; *custom level strike-through; *preserves original spacing/punctuation;\n[nerdy]\n+all html output now escaped through Tag.stringify(); +implemented all functions as classes; +font size now saved between sessions; +kids now displays levelstats";
     const versionInfo = document.getElementById("version-no");
     versionInfo.innerHTML = `Version ${versionNumber}`;
     versionInfo.title += `\n${versionDetails}.`;
